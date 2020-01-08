04e5888df94ebdaf0cae4de80ccc28ea0de477c9
Ambonmibable <48598311+ambonmibable@users.noreply.github.com>
2020-01-07 19:58:39 -0600

    Update interaction.lua

--- modules/interaction.lua
+++ modules/interaction.lua
@@ -3,27 +3,28 @@ return {
     local cmd, arg = string.match(msg.content, '(%S+) (.*)')
     
     if msg.content == "hug" then
+			print("attempt")
         msg:reply{
           embed={
             title="Interaction",
-		description="Bot Interaction",
-		author={
-			name=c.user.username,
-			icon_url=c.member.avatarURL
-		},
-		fields = {
-			{
-				name = "test",
-				value = "nnn",
-				inline = false
-			}
-		},
-		footer = {
-			text = ":)"
-		},
-		color = 0x000000 -- hex color code
-          }
-        }
+						description="Bot Interaction",
+						author={
+							name=c.user.username,
+							icon_url=c.member.avatarURL
+						},
+					fields = {
+						{
+							name = "test",
+							value = "nnn",
+							inline = false
+						}
+					},
+					footer = {
+						text = ":)"
+					},
+					color = 0x000000 -- hex color code
+								}
+							}
       end
    --TODO
    --[[
