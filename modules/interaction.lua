fab6b96c1c7a3d9b4db112b3f3ed07d29afd5a10
Ambonmibable <48598311+ambonmibable@users.noreply.github.com>
2020-01-07 19:21:16 -0600

    Update interaction.lua

--- modules/interaction.lua
+++ modules/interaction.lua
@@ -1,5 +1,19 @@
+local c = require("discordia").Client()
 return {
   messageCreated = function(msg)
+    local cmd, arg = string.match(msg.content, '(%S+) (.*)')
+    
+    if msg.content == "!!hug" then
+        msg:reply{
+          embed={
+            title="Interaction",
+						description="Bot Interaction",
+						author={
+							name=c.user.username,
+							icon_url=c.member.avatarURL
+          }
+        }
+      end
    --TODO
    --[[
     Add Hug
