cd88b47887c83a0875483c8ca1f2d4c8a56ee8f2
Wintermourn <48598311+ambonmibable@users.noreply.github.com>
2020-03-04 14:55:19 -0600

    Update xp.lua

--- modules/xp.lua
+++ modules/xp.lua
@@ -1,5 +1,5 @@
 return {messageCreate = function(m)
   if not save.gett("xp") then save.add("xp") end
   save.set("xp", m.author.id, save.get("xp",m.author.id,0)+1)
-  if m.content == "xm exp" then m:reply("xp is ".. save.get("xp",m.author.id,0) end
+  if m.content == "+!!exp" then m:reply("xp is ".. save.get("xp",m.author.id,0) end
 end}
