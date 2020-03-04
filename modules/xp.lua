de5614236266f868f321619df15f33f58105de06
Wintermourn <48598311+ambonmibable@users.noreply.github.com>
2020-03-04 17:09:01 -0600

    Update xp.lua

--- modules/xp.lua
+++ modules/xp.lua
@@ -1,5 +1,10 @@
 return {messageCreate = function(m)
+  if not save.gett("level") then save.add("level") save.set("level", m.author.id, save.get("level",m.author.id,1)) end
   if not save.gett("xp") then save.add("xp") end
   save.set("xp", m.author.id, save.get("xp",m.author.id,0)+1)
-  if m.content == "+!!exp" then m:reply("xp is ".. save.get("xp",m.author.id,0)) end
+  if save.get("xp",m.author.id) == save.get("level,m.author.id,1)*10 then
+    save.set("xp", m.author.id, save.get("xp",m.author.id,0)-save.get("level,m.author.id,1)*10)
+    save.set("level", m.author.id, save.get("level",m.author.id,1)+1)
+    m:reply("level up!")
+  end
 end}
